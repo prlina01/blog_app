843c6296234eae1ccbd7e00a6e66b5918c9e432b
petar <prlinap@gmail.com>
2018-08-07 06:51:12 +0200

    Added working auth buttons in the nav

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,3 @@
 class ApplicationController < ActionController::Base
-
-  before_action :configure_permitted_parameters
-
-  def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name])
-    devise_parameter_sanitizer.permit(:account_update, keys: [:first_name, :last_name])
-  end
+  include DeviseWhitelist
 end
--- app/models/concerns/buttons_for_auth.rb
+++
@@ -1,9 +0,0 @@
-module ButtonsForAuth
-  def auth_buttons 
-    if current_user
-      link_to "Log out", destroy_user_session_path, method: :delete
-      link_to "Log In", new_user_session_path
-      link_to "Register", new_user_registration_path
-    end 
-  end
-end
